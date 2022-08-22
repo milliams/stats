57dd928d5235c763e0037280c9ae917d21495b86
Matt Williams <matt@milliams.com>
2022-08-22 18:25:10 +0100

    Calculate mean for now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io::{self, BufRead};
+
+fn get_single_column() -> Vec<f64> {
+    let stdin = io::stdin();
+    let mut data: Vec<f64> = vec![];
+    for line in stdin.lock().lines() {
+        let line_text = match line {
+            Ok(line) => line,
+            Err(err) => panic!("IO error: {}", err),
+        };
+        data.push(line_text.parse::<f64>()
+            .expect(&format!("ERROR: Could not parse '{}' as an f64", line_text)));
+    }
+    data
+}
+
 fn main() {
-    println!("stats");
+    let data = get_single_column();
+    let sum: f64 = data.iter().sum();
+    let count = data.len();
+    let mean = sum / count as f64;
+    println!("{}", mean);
 }
