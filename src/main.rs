bebdb962c015dd03da50bf12659c4f69a2f94545
Matt Williams <matt@milliams.com>
2022-08-23 10:53:31 +0100

    Add multiple commands and arg parser

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,32 @@
 use std::io::{self, BufRead};
 
+use clap::{Parser, Subcommand};
+
+/// Simple stats calculator
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Calculates the mean of the data
+    Mean {
+    },
+    /// Calculates the sum of the data
+    Sum {
+    },
+    /// Calculates the maximum of the data
+    Max {
+    },
+    /// Calculates the minimum of the data
+    Min {
+    },
+}
+
 fn get_single_column() -> Vec<f64> {
     let stdin = io::stdin();
     let mut data: Vec<f64> = vec![];
@@ -15,9 +42,30 @@ fn get_single_column() -> Vec<f64> {
 }
 
 fn main() {
-    let data = get_single_column();
-    let sum: f64 = data.iter().sum();
-    let count = data.len();
-    let mean = sum / count as f64;
-    println!("{}", mean);
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Mean{} => {
+            let data = get_single_column();
+            let sum: f64 = data.iter().sum();
+            let count = data.len();
+            let mean = sum / count as f64;
+            println!("{}", mean);
+        },
+        Commands::Sum{} => {
+            let data = get_single_column();
+            let sum: f64 = data.iter().sum();
+            println!("{}", sum);
+        },
+        Commands::Max{} => {
+            let data = get_single_column();
+            let max: f64 = data.iter().fold(f64::NEG_INFINITY, |a, &b| a.max(b));
+            println!("{}", max);
+        },
+        Commands::Min{} => {
+            let data = get_single_column();
+            let min: f64 = data.iter().fold(f64::INFINITY, |a, &b| a.min(b));
+            println!("{}", min);
+        },
+    }
 }
